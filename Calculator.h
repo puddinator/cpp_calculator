1a374eeaec5a58198ed46c908fe333e7f3bd7732
James Gao <jamesg@gatech.edu>
2024-04-21 17:16:26 -0400

    Calculator class

---
+++ Calculator.h
@@ -0,0 +1,9 @@
+#ifndef CALCULATOR__CALCULATOR_H
+#define CALCULATOR__CALCULATOR_H
+
+class Calculator {
+public:
+	double Calculate(double x, char oper, double y);
+};
+
+#endif //CALCULATOR__CALCULATOR_H
